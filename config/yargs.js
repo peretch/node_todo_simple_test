7bf0cf183c537b86687a4bc5c6cfe17e1ec4cc0f
peretch <sebastian@peretch.com>
2020-02-26 14:28:16 -0300

    Reduced code

--- config/yargs.js
+++ config/yargs.js
@@ -1,45 +1,26 @@
-const createOptions = {
-    description: {
-        demand: true,
-        alias: 'd',
-        description: 'Task content'
-    }
-}
+const description = {
+    demand: true,
+    alias: 'd',
+    description: 'Task content'
+};
 
-const updateOptions = {
-    description: {
-        demand: true,
-        alias: 'd',
-        description: 'Task content'
-    },
-    completed: {
-        default: true,
-        alias: 'c',
-        description: 'Task completed (default=true)'
-    }
-}
+const completed = {
+    default: true,
+    alias: 'c',
+    description: 'Task completed (default=true)'
+};
 
-const deleteOptions = {
-    description: {
-        demand: true,
-        alias: 'd',
-        description: 'Task content'
-    }
-}
-
-const list_options = {
-    all: {
-        default: false,
-        alias: 's',
-        description: 'Get all tasks (default=false)'
-    }
-}
+const all = {
+    default: false,
+    alias: 's',
+    description: 'Get all tasks (default=false)'
+};
 
 const argv = require('yargs')
-    .command('create', 'Creates one task', createOptions)
-    .command('update', 'Updates one task', updateOptions)
-    .command('delete', 'Deletes one task', deleteOptions)
-    .command('list', 'Returns all tasks unfinished', list_options)
+    .command('create', 'Creates one task', { description })
+    .command('update', 'Updates one task', { description, completed })
+    .command('delete', 'Deletes one task', { description })
+    .command('list', 'Returns all tasks unfinished', { all })
     .help()
     .argv;
 
